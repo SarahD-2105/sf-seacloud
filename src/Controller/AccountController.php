6178528385340c7240d754d1acd4522b760e70a6
Sarah Doucet <sarahdoucet1997@outlook.com>
2021-06-01 16:37:30 +0200

    Continuation des ajouts de templates, routes et liens vers les pages respectives.

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,14 +4,17 @@
 namespace App\Controller;
 
 
-use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/account")
+ */
 class AccountController extends AbstractController
 {
     /**
-     * @Route ('/')
-     *
+     * @Route("/")
      */
     public function index(): Response
     {
--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -3,20 +3,70 @@
 
 namespace App\Controller;
 
-use ContainerPs7OxBH\getContainer_EnvVarProcessorService;
-use http\Env\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
     /**
-     * @Route("/", name="page-home")
+     * @Route("/", name="page_home")
      *
      * @return Response
      */
     public function home(): Response
     {
-        return new Response('Home page');
+        return $this->render('Page/home.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="page_about")
+     *
+     * @return Response
+     */
+    public function about(): Response
+    {
+        return $this->render('Page/about.html.twig');
+    }
+
+    /**
+     * @Route("/services", name="page_services")
+     *
+     * @return Response
+     */
+    public function services(): Response
+    {
+        return $this->render('Page/services.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="page_contact")
+     *
+     * @return Response
+     */
+    public function contact(): Response
+    {
+        return $this->render('Page/contact.html.twig');
+    }
+
+    /**
+     * @Route("/sign-up", name="page_sign-up")
+     *
+     * @return Response
+     */
+    public function signUp(): Response
+    {
+        return $this->render('Page/sign-up.html.twig');
+    }
+
+    /**
+     * @Route("/sign-in", name="page_sign-in")
+     *
+     * @return Response
+     */
+    public function signIn(): Response
+    {
+        return $this->render('Page/sign-in.html.twig');
     }
 }
 
